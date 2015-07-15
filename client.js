b4188fa1c1be3a950e6f5a6d760dd80a856642bd
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-07-16 01:01:14 +0700

    Switch to SockJS

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-//var jpeg = require('jpeg-js');
-
-var socket = io();
+var socket = new SockJS('/echo');
+//var socket = io();
+var keymap = require('./keymap');
 var canvas = document.getElementById('canvas-screen');
 var context = canvas.getContext('2d');
 
@@ -8,108 +8,19 @@ var shifting = false;
 var ctrling = false;
 var alting = false;
 
-var keymap = {
-    8: 'backspace'
-  , 9: 'tab'
-  , 13: 'ret'
-  , 16: 'shift'
-  , 17: 'ctrl'
-  , 18: 'alt'
-  , 19: '?'// pause
-  , 20: 'caps_lock'
-  , 27: 'esc'
-  , 32: 'spc'
-  , 33: 'pgup'
-  , 34: 'pgdn'
-  , 35: 'end'
-  , 36: 'home'
-  , 37: 'left'
-  , 38: 'up'
-  , 39: 'right'
-  , 40: 'down'
-  , 44: 'print'
-  , 45: 'insert'
-  , 46: 'delete'
-  , 48: '0'
-  , 49: '1'
-  , 50: '2'
-  , 51: '3'
-  , 52: '4'
-  , 53: '5'
-  , 54: '6'
-  , 55: '7'
-  , 56: '8'
-  , 57: '9'
-  , 59: 'semicolon'
-  , 61: 'equal'
-  , 65: 'a'
-  , 66: 'b'
-  , 67: 'c'
-  , 68: 'd'
-  , 69: 'e'
-  , 70: 'f'
-  , 71: 'g'
-  , 72: 'h'
-  , 73: 'i'
-  , 74: 'j'
-  , 75: 'k'
-  , 76: 'l'
-  , 77: 'm'
-  , 78: 'n'
-  , 79: 'o'
-  , 80: 'p'
-  , 81: 'q'
-  , 82: 'r'
-  , 83: 's'
-  , 84: 't'
-  , 85: 'u'
-  , 86: 'v'
-  , 87: 'w'
-  , 88: 'x'
-  , 89: 'y'
-  , 90: 'z'
-  , 91: 'ctrl' // left command
-  , 93: 'ctrl' // right command
-  , 107: 'equal'
-  , 109: 'minus'
-  , 112: 'f1'
-  , 113: 'f2'
-  , 114: 'f3'
-  , 115: 'f4'
-  , 116: 'f5'
-  , 117: 'f6'
-  , 118: 'f7'
-  , 119: 'f8'
-  , 120: 'f9'
-  , 121: 'f10'
-  , 122: 'f11'
-  , 123: 'f12'
-  , 144: 'num_lock'
-  , 145: 'scroll_lock'
-  , 186: 'semicolon'
-  , 187: 'equal'
-  , 188: 'comma'
-  , 189: 'minus'
-  , 190: 'dot'
-  , 191: 'slash'
-  , 192: 'apostrophe'
-  , 219: 'bracket_left'
-  , 220: 'backslash'
-  , 221: 'bracket_right'
-  , 222: '\''
-  , 224: 'ctrl' // command in firefox
-};
-
 function qemukey(keycode) {
   var mapping = keymap[keycode];
   if (!mapping) return null;
 
   if (mapping == 'shift') {
-    shifting = true; return null;
+    shifting = true;
+    return null;
   } else if (mapping == 'ctrl') {
-    ctrling = true; return null;
+    ctrling = true;
+    return null;
   } else if (mapping == 'alt') {
-    alting = true; return null;
+    alting = true;
+    return null;
   }
 
   var prefix = '';
@@ -134,7 +45,7 @@ $('#canvas-screen').mousedown(function(e) {
     button: button
   };
 
-  socket.emit('pointer', payload);
+  //socket.emit('pointer', payload);
 });
 
 $('#canvas-screen').mouseup(function(e) {
@@ -149,7 +60,7 @@ $('#canvas-screen').mouseup(function(e) {
     button: 0
   };
 
-  socket.emit('pointer', payload);
+  //socket.emit('pointer', payload);
 });
 
 $('#canvas-screen').mousemove(function(e) {
@@ -166,13 +77,13 @@ $('#canvas-screen').mousemove(function(e) {
     button: button
   };
 
-  socket.emit('pointer', payload);
+  //socket.emit('pointer', payload);
 });
 
 $(document).keydown(function(e) {
   e.preventDefault();
   var key = qemukey(e.which);
-  if (key) socket.emit('key', key);
+  //if (key) socket.emit('key', key);
 });
 
 $(document).keyup(function(e) {
@@ -181,32 +92,53 @@ $(document).keyup(function(e) {
 
   if (mapping == 'shift') {
     shifting = false;
-  } else if (mapping == 'ctrl'){
+  } else if (mapping == 'ctrl') {
     ctrling = false;
   } else if (mapping == 'alt') {
     alting = false;
   }
 });
 
-socket.on('frame', function(payload) {
-  var data = payload.frame.data;
-  var image = context.createImageData(payload.width, payload.height);
+// socket.on('frame', function(payload) {
+//   var data = payload.frame;
+//   var image = context.createImageData(payload.width, payload.height);
+//
+//   for (var s = 0, d = 0, len = payload.width * payload.height * 4; d < len; ++d) {
+//     if (d % 4 == 3) {
+//       image.data[d] = 0xff;
+//     } else {
+//       image.data[d] = data[s];
+//       ++s;
+//     }
+//   }
+//
+//   context.putImageData(image, payload.x, payload.y);
+// });
 
-  for (var s = 0, d = 0, len = payload.width * payload.height * 4; d < len; ++d) {
-    if (d % 4 == 3) {
-      image.data[d] = 0xff;
-    } else {
-      image.data[d] = data[s];
-      ++s;
-    }
-  }
+// socket.on('count', function(count) {
+//   $('#online-count').text(count);
+// });
 
-  context.putImageData(image, payload.x, payload.y);
-});
 
-socket.on('count', function(count) {
-  $('#online-count').text(count);
-});
+socket.onopen = function() {
+  console.log('open');
+
+  var send = {
+    message: 'Hi!',
+    username: 'sbekti'
+  };
+
+  socket.send(JSON.stringify(send));
+};
+
+socket.onclose = function() {
+  console.log('close');
+};
+
+socket.onmessage = function(e) {
+  var content = JSON.parse(e.data);
+  console.log(content);
+};
 
 var x = canvas.width / 2;
 var y = canvas.height / 2;
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
+var sockjs = require('sockjs');
 var path = require('path');
 var rfb = require('rfb2');
 var net = require('net');
 
-var onlineCount = 0;
+var clients = {};
+//var onlineCount = 0;
+
+var echo = sockjs.createServer();
 
 var r = rfb.createConnection({
-  host: process.env.RFB_HOST,
-  port: process.env.RFB_PORT,
+  host: process.env.RFB_HOST || 'bekti.io',
+  port: process.env.RFB_PORT || 5902,
   password: process.env.RFB_PASSWORD
 });
 
-var tcp = net.connect({host: process.env.RFB_HOST, port: process.env.RFB_MONITOR_PORT});
+var tcp = net.connect({
+  host: process.env.RFB_HOST || 'bekti.io',
+  port: process.env.RFB_MONITOR_PORT || 4444
+});
 
 r.on('connect', function() {
   console.log('Remote screen name: ' + r.title + ' width:' + r.width + ' height: ' + r.height);
@@ -23,7 +30,7 @@ r.on('connect', function() {
 r.on('rect', function(rect) {
   if (rect.encoding != 0) return;
 
-  var frameBuffer = new Buffer(rect.width * rect.height * 3);
+  const frameBuffer = new Uint8Array(rect.width * rect.height * 3);
 
   for (var i = 0, o = 0; i < rect.data.length; i += 4) {
     frameBuffer[o++] = rect.data[i + 2];
@@ -31,42 +38,63 @@ r.on('rect', function(rect) {
     frameBuffer[o++] = rect.data[i];
   }
 
-  var frame = frameBuffer.toJSON(frameBuffer);
-
-  var payload = {
-    frame: frame,
+  const payload = {
+    frame: frameBuffer,
     x: rect.x,
     y: rect.y,
     width: rect.width,
     height: rect.height
   }
 
-  io.emit('frame', payload);
+  //io.emit('frame', payload);
 });
 
-io.on('connection', function(socket) {
-  ++onlineCount;
-  io.emit('count', onlineCount);
-  r.requestUpdate(false, 0, 0, r.width, r.height);
-
-  socket.on('pointer', function(payload) {
-    r.pointerEvent(payload.x, payload.y, payload.button);
-  });
-
-  socket.on('key', function(key) {
-    tcp.write('sendkey ' + key + '\n');
-  });
-
-  socket.on('disconnect', function(payload) {
-    --onlineCount;
-    io.emit('count', onlineCount);
-  });
-});
+// io.on('connection', function(socket) {
+//   ++onlineCount;
+//   io.emit('count', onlineCount);
+//   r.requestUpdate(false, 0, 0, r.width, r.height);
+//
+//   socket.on('pointer', function(payload) {
+//     r.pointerEvent(payload.x, payload.y, payload.button);
+//   });
+//
+//   socket.on('key', function(key) {
+//     tcp.write('sendkey ' + key + '\n');
+//   });
+//
+//   socket.on('disconnect', function(payload) {
+//     --onlineCount;
+//     io.emit('count', onlineCount);
+//   });
+// });
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+function broadcast(message) {
+  for (var client in clients) {
+    clients[client].write(JSON.stringify(message));
+  }
+}
+
+echo.on('connection', function(conn) {
+  clients[conn.id] = conn;
+
+  conn.on('data', function(message) {
+    console.log(message);
+    broadcast(JSON.parse(message));
+  });
+
+  conn.on('close', function() {
+    delete clients[conn.id];
+  });
+});
+
+echo.installHandlers(http, {
+  prefix: '/echo'
 });
 
 http.listen(5000, function() {
